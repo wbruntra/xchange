bcb17e9a15555b9a6f21f8cfd55814845583c4ee
William Bruntrager <w.bruntrager@gmail.com>
2016-09-28 10:57:04 -0400

    have userlist

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,10 +1,19 @@
 var socket = io();
-$('form').submit(function(){
+$('form#chat-form').submit(function(){
   socket.emit('chat message', $('#m').val());
   $('#m').val('');
   return false;
 });
 
+$('form#name').submit(function() {
+  socket.emit('join', $('#n').val());
+  return false;
+});
+
 socket.on('chat message', function(msg) {
   console.log(msg);
+});
+
+socket.on('join', function(name) {
+  console.log('somebody is here');
 });
--- sockets/base.js
+++ sockets/base.js
@@ -1,12 +1,35 @@
 module.exports = function(io) {
+  var userList = [];
+  var removeUser = function(name) {
+    // Find and remove item from an array
+    var i = userList.indexOf(name);
+    if(i != -1) {
+      userList.splice(i, 1);
+    }
+  }
   io.on('connection', function(socket) {
-    console.log("a user connected, dude")
-    socket.on('disconnect', function(){
-      console.log('user disconnected');
+    var userName;
+    console.log("a user connected, pal");
+
+    socket.send()
+
+    socket.on('join', function(name) {
+      removeUser(userName);
+      userName = name;
+      userList.push(userName);
+      console.log(userList);
     });
+
+    socket.on('disconnect', function(){
+      console.log(userName + ' disconnected');
+      removeUser(userName);
+      console.log(userList);
+    });
+
     socket.on('chat message', function(msg) {
       console.log('message: '+ msg);
     });
+
     socket.on('chat message', function(msg) {
       io.emit('chat message', msg);
     });
